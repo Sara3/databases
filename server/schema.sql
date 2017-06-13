2b2acbac607cd56cf62dbc84b689752150ccda0e
Hack Reactor Student <student@hackreactor.com>
2017-06-12 20:57:36 -0700

    init

--- server/schema.sql
+++ server/schema.sql
@@ -2,16 +2,34 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+CREATE TABLE users (
+  id int NOT NULL,
+  name varchar(50),
+  PRIMARY KEY(id)
 );
 
-/* Create other tables and define schemas for them here! */
+CREATE TABLE rooms (
+  id int NOT NULL,
+  name varchar(50),
+  PRIMARY KEY(id)
+);
 
+CREATE TABLE messages (
+  /* Describe your table here.*/
+  id int NOT NULL,
+  text varchar(1000),
+  userId int,
+  roomId int,
+  PRIMARY KEY(id),
+  FOREIGN KEY(userId) REFERENCES users(id),
+  FOREIGN KEY(roomId) REFERENCES rooms(id)
+);
+
+-- why cant we get the id of rooms and users in the mesages table
+/* Create other tables and define schemas for them here! */
 
 
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
  *  to create the database and the tables.*/
-
